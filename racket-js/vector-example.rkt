72f1ae8e6a0637ddd601df436493eeb68173da31
Allen Hsu <allenhsu4390@gmail.com>
2018-05-10 21:58:23 -0700

    update racket-js

--- racket-js/vector-example.rkt
+++ racket-js/vector-example.rkt
@@ -1,18 +1,4 @@
-#lang racket
-
-(require racket-js)
-
-(module/js vector-example
-
-  (require basejs)
-  (require vectorjs)
-  (require forjs)
-
-  (displayln (for/first ([i (vector 1 2 3 4 5)] #:when (> i 25)) i))
-  (displayln (for/vector ([i (vector 1 2 3 4 5)] #:when (> i 2)) i))
-  (displayln (for/vector ([i (vector "hi" "bye" "")] #:when (not (string=? "" i))) i))
-  (displayln (let* ([i 10] [j (+ i 10)]) (+ j i)))
-
-  )
-
-(send vector-example write-to-file: "vector-example.js")
+(displayln (for/first ([i (vector 1 2 3 4 5)] #:when (> i 25)) i))
+(displayln (for/vector ([i (vector 1 2 3 4 5)] #:when (> i 2)) i))
+(displayln (for/vector ([i (vector "hi" "bye" "")] #:when (not (string=? "" i))) i))
+(displayln (let* ([i 10] [j (+ i 10)]) (+ j i)))
